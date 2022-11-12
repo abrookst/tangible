45df31d684a10ae949e971ca9789735467cc0a80
Chloe MacGregor <chloemacgregor29@gmail.com>
2022-11-12 12:16:50 -0500

    added test message to verify dev environment works

--- calculate.ts
+++ calculate.ts
@@ -1,4 +1,6 @@
 // Returns kg of CO2
-function embodied_carbon_of(material, mass) {
+function embodied_carbon_of(material: any, mass: number): number {
     return 0;
 }
+
+console.log("hello energyhack!");
